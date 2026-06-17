97c30c931771bda97c30d5c200b92c17be91c149
Miguel Angel Gomez Hurtado <150874611+Miguel26112001@users.noreply.github.com>
2026-06-17 11:27:14 -0500

    tests(groups): add unit tests for GroupQueryServiceImpl
    Added tests for `GroupQueryServiceImpl` to validate `GetGroupByIdQuery` handling, covering scenarios for existing groups and groups not found.

---
+++ src/test/java/com/collabrium/groups/management/application/internal/queryservices/GroupQueryServiceImplTest.java
@@ -0,0 +1,62 @@
+package com.collabrium.groups.management.application.internal.queryservices;
+
+import com.collabrium.groups.management.application.internal.outboundservices.ports.IamQueryPort;
+import com.collabrium.groups.management.domain.model.aggregates.Group;
+import com.collabrium.groups.management.domain.model.queries.GetGroupByIdQuery;
+import com.collabrium.groups.management.infrastructure.persistence.jpa.repositories.GroupRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class GroupQueryServiceImplTest {
+
+  @Mock
+  GroupRepository groupRepository;
+
+  @Mock
+  IamQueryPort iamQueryPort;
+
+  @InjectMocks
+  GroupQueryServiceImpl service;
+
+  @Test
+  @DisplayName("handle(GetGroupByIdQuery) - existing group: returns group")
+  void handle_getGroupById_existingGroup_returnsGroup() {
+    // Arrange
+    var group = mock(Group.class);
+    var query = new GetGroupByIdQuery(1L);
+
+    when(groupRepository.findById(1L)).thenReturn(Optional.of(group));
+
+    // Act
+    var result = service.handle(query);
+
+    // Assert
+    assertThat(result).containsSame(group);
+  }
+
+  @Test
+  @DisplayName("handle(GetGroupByIdQuery) - group not found: returns empty")
+  void handle_getGroupById_groupNotFound_returnsEmpty() {
+    // Arrange
+    var query = new GetGroupByIdQuery(1L);
+
+    when(groupRepository.findById(1L)).thenReturn(Optional.empty());
+
+    // Act
+    var result = service.handle(query);
+
+    // Assert
+    assertThat(result).isEmpty();
+  }
+}
